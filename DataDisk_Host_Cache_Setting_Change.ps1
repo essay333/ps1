ffa1d53b65157bc23e1517b5feb31e5ac6a0eda1
JungA <JungA@dhlee-NB01.zenithn.com>
2020-07-03 00:38:57 +0900

    data disk host cache setting change powershell script

---
+++ DataDisk_Host_Cache_Setting_Change.ps1
@@ -0,0 +1,17 @@
+# 2020-07-03
+# Azure VM의 Data Disk의 Host Cache 설정을 변경하는 PowerShell Script
+# Data Disk가 여러 개인 경우, 모든 Data Disk의 Host Cache 설정을 동일하게 설정
+
+$vmName = "yourVmName"
+$vmResourceGroup ="yourResourceGroupName"
+$hostCaching = "None" # or ReadWrite or ReadOnly
+
+$vm = Get-AzVM -ResourceGroupName $vmResourceGroup -Name $vmName
+$dataDisks = $vm.StorageProfile.DataDisks
+
+
+foreach ( $disk in $dataDisks) {
+    Set-AzVMDataDisk -VM $vm -Name $disk.Name -Caching $hostCaching
+}
+
+Update-azvm -VM $vm -ResourceGroupName $vmResourceGroup
