9e91ca67780bd8342ec1c00e0b9bec72f49318e3
JungA <JungA@dhlee-NB01.zenithn.com>
2020-07-03 00:59:31 +0900

    Disk 사이즈에 따른 제약 추가

--- DataDisk_Host_Cache_Setting_Change.ps1
+++ DataDisk_Host_Cache_Setting_Change.ps1
@@ -1,6 +1,7 @@
 # 2020-07-03
 # Azure VM의 Data Disk의 Host Cache 설정을 변경하는 PowerShell Script
 # Data Disk가 여러 개인 경우, 모든 Data Disk의 Host Cache 설정을 동일하게 설정
+# 중요 - Data Disk가 4TB 이상인 경우, Host Cache는 None 으로만 설정 가능 
 
 $vmName = "yourVmName"
 $vmResourceGroup ="yourResourceGroupName"
